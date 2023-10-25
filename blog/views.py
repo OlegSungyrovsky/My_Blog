2b3dff5ff6c55658d0838e43845e97c4468dd208
Sungyrov <andreisungurovsky@yandex.ru>
2023-10-25 18:01:44 +0300

    Integrated tags, update templates for work with tags, Add custom tags, new filter for markdown formation

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,19 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
+from django.db.models import Count
+from taggit.models import Tag
 
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
@@ -22,7 +28,10 @@ def post_list(request):
     return render(
         request,
         'blog/post/list.html',
-        {'posts': posts}
+        {
+            'posts': posts,
+            'tag': tag
+         }
     )
 
 
@@ -37,13 +46,18 @@ def post_detail(request, year, month, day, post):
     )
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')
     return render(
         request,
         'blog/post/detail.html',
         {
             'post': post,
             'form': form,
-            'comments': comments
+            'comments': comments,
+            'similar_posts': similar_posts
         }
     )
 
